d9a6f6889b9a75a0b69e67bafad63284a577f971
LAPTOP-1QSKFQUC\ben.xia <bo.xia@huifu.com>
2022-10-30 09:45:06 +0800

    日志

---
+++ src/main/java/com/ben/java/core/log/Log4jTest.java
@@ -0,0 +1,15 @@
+package com.ben.java.core.log;
+
+import org.junit.Test;
+
+/**
+ * @author: ben.xia
+ * @desc:
+ * @date: 2022/10/30
+ */
+public class Log4jTest {
+    @Test
+    public void testLog4j01(){
+
+    }
+}
