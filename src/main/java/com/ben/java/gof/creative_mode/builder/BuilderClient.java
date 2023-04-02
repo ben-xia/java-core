e452260ff0e78f0633a6f2f709fb9bf1808ac5c1
LAPTOP-1QSKFQUC\ben.xia <bo.xia@huifu.com>
2023-04-02 12:16:31 +0800

    Builder

---
+++ src/main/java/com/ben/java/gof/creative_mode/builder/BuilderClient.java
@@ -0,0 +1,23 @@
+package com.ben.java.gof.creative_mode.builder;
+
+
+import org.apache.ibatis.mapping.CacheBuilder;
+
+/**
+ * jdk的StringBuilder
+ * mybatis的CacheBuilder,SqlSessionFactoryBuilder
+ */
+public class BuilderClient {
+
+    public static void main(String[] args) {
+
+        Person person = PersonBuilder.builder()
+                        .address("上海") // chain 调用
+                        .name("张三")
+                        .age(23)
+                        .gender("男")
+                        .hobby("骑行")
+                        .build();
+        System.out.println(person);
+    }
+}
